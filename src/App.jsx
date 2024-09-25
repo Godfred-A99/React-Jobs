59bcf4e165ae2579f009e531a2e0b49c8ba5f032
Godfred-A99 <godfredadjetey99@gmail.com>
2024-09-25 17:22:12 +0000

    add jobs files

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+import Hero from "./componentes/Hero"
+import HomeCards from "./componentes/homeCards"
+import JobListings from "./componentes/JobListings"
+import Navbar from "./componentes/Navbar"
+
+
+
+
+const App = () => {
+ 
+
+  return (
+    <>
+      <Navbar  />
+      <Hero />
+      <HomeCards />
+      <JobListings  />
+
+    
+    
+
+    
+    
+
+    
+    
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+    </>
+  )
+}
+
+export default App 
