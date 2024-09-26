c536e70abe213c2fa015835309f2ef6859e8ed28
Godfred-A99 <godfredadjetey99@gmail.com>
2024-09-26 16:41:09 +0000

    add json-server

--- src/Page/JobsPage.jsx
+++ src/Page/JobsPage.jsx
@@ -1,5 +1,8 @@
+import JobListings from "../components/JobListings";
 const JobsPage = () => {
-  return <div>JobPage</div>;
+  return <section className="bg-blue-500 px-4 py-6">
+    <JobListings />
+  </section>;
 };
 
 export default JobsPage;
