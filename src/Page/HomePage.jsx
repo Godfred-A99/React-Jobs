220666c85b2590ce113c19ad642c25db3d43f27d
Godfred-A99 <godfredadjetey99@gmail.com>
2024-09-26 02:16:03 +0000

    added page and layouts file

---
+++ src/Page/HomePage.jsx
@@ -0,0 +1,20 @@
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
+
+
+// This is the home page component
+
+const HomePage = () => {
+  return (
+    <>
+          <Hero />
+          <HomeCards />
+          <JobListings />
+          <ViewAllJobs />
+    </>
+  );
+};
+
+export default HomePage;
