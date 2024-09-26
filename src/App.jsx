98efa5d952e0661b5719583b59caa217e7dc4f25
Godfred-A99 <godfredadjetey99@gmail.com>
2024-09-26 15:42:29 +0000

    added an error page

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,19 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./Page/HomePage";
 import JobsPage from "./Page/JobsPage";
+import NotFoundPage from "./Page/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
+    <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
-      
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
-)
+);
 const App = () => {
-  return <RouterProvider  router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
